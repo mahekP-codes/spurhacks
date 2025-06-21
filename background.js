62e72d903c3290ed9972558fd49f01fcb945e8a1
Angela <angela.zhou12345@gmail.com>
2025-06-21 19:05:06 -0400

    more styling

--- background.js
+++ background.js
@@ -67,3 +67,17 @@ function convertPatternToRegex(pattern) {
     .replace(/\*/g, ".*")
     .replace(/\//g, "\\/") + "$";
 }
+
+let timerID;
+let timerTime;
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.cmd === 'START_TIMER') {
+    timerTime = new Date(request.when);
+    timerID = setTimeout(() => {
+       // the time is app, alert the user.
+    }, timerTime.getTime() - Date.now());
+  } else if (request.cmd === 'GET_TIME') {
+    sendResponse({ time: timerTime });
+  }
+});
